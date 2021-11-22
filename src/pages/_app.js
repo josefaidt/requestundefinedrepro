57406a145070d829692eece24c065b632fee6be2
josef <josef.aidt@gmail.com>
2021-11-22 14:48:20 -0600

    secure route, fix auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,26 @@
 import { Amplify } from 'aws-amplify'
-import { withAuthenticator } from '@aws-amplify/ui-react'
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 import awsConfig from '../aws-exports'
 
 Amplify.configure(awsConfig)
 
-export default withAuthenticator(function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-})
+// export default withAuthenticator(function MyApp({ Component, pageProps, ...rest }) {
+//   // `rest` contains { user, signOut }
+//   console.log({ Component }) // Component is undefined
+//   return <Component {...pageProps} />
+// })
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+          <Component {...pageProps} />
+        </main>
+      )}
+    </Authenticator>
+  )
+}
